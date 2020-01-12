c303180f4229e3d1f1613199b213208f8fea64ec
Chris Stack <cstack89@gmail.com>
2020-01-12 08:17:41 -0500

    Got it working under a zuul proxy. No security currently.

---
+++ src/main/java/demo/PicSecurityConfig.java
@@ -0,0 +1,27 @@
+package demo;
+
+
+//@Profile("Production")
+//@EnableOAuth2Sso
+//public class PicSecurityConfig extends WebSecurityConfigurerAdapter {
+//
+//	@Override
+//	protected void configure(HttpSecurity http) throws Exception {
+//		// @formatter:off
+//	        http
+////	        .logout().logoutSuccessUrl("/logout").and() 
+////	            .authorizeRequests()
+////	                .antMatchers("/*.png","/*.js.*", "/login", "/static/**","/img/**").permitAll() //"/",
+////	                .anyRequest().authenticated()
+////	                .and()
+////	            .csrf().disable();
+////	        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+////            ;
+//	        
+//	        
+//	        //Attempt to disable
+//	        .httpBasic().disable().formLogin().disable();
+////	              
+//	            // @formatter:on
+//	}
+//}
--- src/main/java/demo/UiApplication.java
+++ src/main/java/demo/UiApplication.java
@@ -2,27 +2,16 @@ package demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.oauth2.client.OAuth2ClientContext;
-import org.springframework.security.oauth2.client.OAuth2RestOperations;
-import org.springframework.security.oauth2.client.OAuth2RestTemplate;
-import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @SpringBootApplication
-@EnableZuulProxy
-@EnableOAuth2Sso
-@EnableDiscoveryClient
+//@EnableZuulProxy
+
+//@EnableDiscoveryClient
 @ComponentScan(basePackages = { "com.fledglingmaker", "demo" })
 // @ComponentScan(basePackages = {"com.fledglingmaker","demo"},basePackageClasses = KeycloakSecurityComponents.class)
 
-public class UiApplication extends WebSecurityConfigurerAdapter {
+public class UiApplication  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UiApplication.class, args);
@@ -32,20 +21,7 @@ public class UiApplication extends WebSecurityConfigurerAdapter {
 	// @Order(-20)
 	// protected static class LoginConfig extends WebSecurityConfigurerAdapter {
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		// @formatter:off
-	        http
-	        .logout().logoutSuccessUrl("/logout").and() 
-	            .authorizeRequests()
-	                .antMatchers("/*.png","/*.js.*", "/login", "/static/**","/img/**").permitAll() //"/",
-	                .anyRequest().authenticated()
-	                .and()
-	            .csrf().disable();
-//	                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-//	                ;
-	            // @formatter:on
-	}
+	
 //
 //	@Bean
 //	public OAuth2RestOperations restTemplate(OAuth2ClientContext clientContext, OAuth2ProtectedResourceDetails resourceDetails) {
